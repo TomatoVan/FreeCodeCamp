fba4efd9ba02853166ce66c891baa9242a51013f
Leo <kovzel.leonid@mail.ru>
2021-11-17 13:27:28 +0300

    Basic Algorithm Scripting

--- ES 6/20.js
+++ ES 6/20.js
@@ -1,7 +1,7 @@
-<html>
+{/* <html>
   <body>
     <!-- Only change code below this line -->
 <script type="module" src="index.js"></script>
     <!-- Only change code above this line -->
   </body>
-</html>
+</html> */}
